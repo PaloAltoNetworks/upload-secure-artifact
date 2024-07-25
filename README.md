9ff3c62982f62bb88458b4434c279bb344bc0a70
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2024-07-25 11:09:45 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+<img src="logo.jpeg" width="250"> 
+
+
+# Upload Artifact Secure
+
+This GitHub Action scans artifacts for secrets using [gitleaks](https://github.com/gitleaks/gitleaks) before uploading them. The upload functionality is done by wrapping the original [upload-artifact](https://github.com/actions/upload-artifact) GitHub action, ensuring that no sensitive information is included in the uploaded artifacts.
+
+## Features
+
+- Compatible with upload-artifact v4
+- Alerts users if any secrets are detected
+- Prevents uploading artifacts if secrets are found
+- Wraps around the `upload-artifact` GitHub action for seamless integration
+
+## Usage
+
+To use this action add it to your GitHub Actions workflow file:
+
+```yaml
+
+    - uses: cider-research-testing/upload-artifact-secure@main
+       with:
+         name: python-build
+     	 path: /output
+
+```
+
+** _Pin your actions for a safer world_
+
+## Inputs
+
+- original upload-artifact inputs can be found [here](https://github.com/actions/upload-artifact?tab=readme-ov-file#inputs)
+
+- scan-only-github-secrets (Optional)
+
+	-	Description: If true, skip the gitleaks secrets scanning and only perform scanning for the runner token in the local .git folder.
+	-	Default: false
+
+## Outputs
+
+-  artifact-id, artifact-url: supplied by the upload-artifact action
+
+
+## Contributing
+
+  Contributions are welcome! Please open an issue or submit a pull request if you have any improvements or suggestions.
+
+## Contact
+
+  For any questions or support, please open an issue on the GitHub repository.
---
+++ index.js
@@ -0,0 +1,109 @@
+const { DefaultArtifactClient } = require("@actions/artifact");
+const fs = require('fs');
+const path = require('path');
+const core = require('@actions/core');
+
+async function main(github, context, artifactName,artifactPath,retentionDays,compressionLevel) {
+  console.log(`Inside main...`);
+  const artifactClient = new DefaultArtifactClient();
+
+  console.log(`Artifact path: ${artifactPath}`);
+  console.log(`Workspace: ${process.env.GITHUB_WORKSPACE}`);
+
+  try {
+    await uploadArtifact(artifactClient, artifactName, artifactPath,retentionDays,compressionLevel);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function uploadArtifact(artifactClient, artifactName, artifactPath,retentionDays,compressionLevel) {
+  if (hasGitFolderWithGitHubRunnerToken(artifactPath)) {
+    throw new Error("Error: Do not upload the GITHUB_TOKEN inside artifacts.");
+  }
+
+  const filesToUpload = await populateFilesWithFullPath(artifactPath);
+
+  await artifactClient.uploadArtifact(
+    artifactName,
+    filesToUpload,
+    process.env.GITHUB_WORKSPACE,
+    { retentionDays: 10 } // Optional: Set retention days
+  );
+}
+
+
+function findGitFolder(startPath) {
+    if (!fs.existsSync(startPath)) {
+        console.log("Start path does not exist.");
+        return null;
+    }
+
+    const files = fs.readdirSync(startPath);
+
+    for (let i = 0; i < files.length; i++) {
+        const filePath = path.join(startPath, files[i]);
+
+        if (files[i] === '.git' && fs.statSync(filePath).isDirectory()) {
+            return filePath;
+        }
+
+        if (fs.statSync(filePath).isDirectory()) {
+            const result = findGitFolder(filePath);
+            if (result) {
+                return result;
+            }
+        }
+    }
+
+    return null;
+}
+
+function hasGitFolderWithGitHubRunnerToken(pathToCheck) {
+  const fs = require('fs');
+  const path = require('path');
+
+  const gitDir = findGitFolder(pathToCheck, '.git');
+  const configFile = path.join(gitDir, 'config');
+  const regex = new RegExp('eC1hY2Nlc3MtdG9rZW46Z2hz', 'i');
+
+  try {
+    if (fs.existsSync(gitDir) && fs.existsSync(configFile)) {
+      const configContent = fs.readFileSync(configFile, 'utf-8');
+      return regex.test(configContent);
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error('Error checking Git config:', error);
+    return false;
+  }
+}
+
+async function populateFilesWithFullPath(rootPath) {
+  const fs = require('fs').promises; // Use promises for cleaner async/await usage
+  const path = require('path');
+  const files = [];
+
+  const dirEntries = await fs.readdir(rootPath);
+  for (const fileName of dirEntries) {
+    const filePath = path.join(rootPath, fileName);
+
+    const stats = await fs.stat(filePath);
+    if (stats.isFile()) {
+      files.push(filePath);
+    } else if (stats.isDirectory()) {
+      // Recursively collect files from subdirectories
+      files.push(...(await populateFilesWithFullPath(filePath)));
+    }
+  }
+
+  return files;
+}
+
+module.exports = function ({ github, context , artifactName,artifactPath,retentionDays,compressionLevel }) { 
+   main(github, context, artifactName,artifactPath,retentionDays,compressionLevel);
+}
+
+
+
